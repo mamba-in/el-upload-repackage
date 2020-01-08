c3611d45f9b52d54fab8bd7ff1c08418fae67705
wanglei <1290898818@qq.com>
2020-01-08 10:22:00 +0800

    el-upload-repackage github pages

--- src/plugins/element.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import { Button, Upload, Message, MessageBox } from 'element-ui'
-
-Vue.use(Button)
-Vue.use(Upload)
-Vue.prototype.$message = Message
-Vue.prototype.$confirm = MessageBox.confirm
